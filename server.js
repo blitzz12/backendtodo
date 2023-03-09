5da12ba86920acfc5b6c8ef2b61ef307a09e631b
blitzz12 <carl.brooks1@gmail.com>
2023-03-08 18:38:24 -0700

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const errorHandler = require('./middlewares/errorMiddleware')
 
 const app = express()
 connectDB()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
@@ -15,14 +15,10 @@ app.use(cors())
 app.use('/api/todos', require('./routes/todoRoutes'))
 app.use('/api/user', require('./routes/userRoutes'))
 
+ if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-// } else {
-//   app.get('/', (req, res) => res.send('Please set to production'))
-// }
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))) } else {   app.get('/', (req, res) => res.send('Please set to production')) }
 
 app.use(errorHandler)
 
