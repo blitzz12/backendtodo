ef0e53fd81c13e08e2a3cbc3a30cd973220d76f7
blitzz12 <carl.brooks1@gmail.com>
2023-03-09 22:46:54 -0700

    next

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ connectDB()
 const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors( {origin:true, credentials: true}))
+app.use(cors())
 
 app.use('/api/todos', require('./api/todoRoutes'))
 app.use('/api/user', require('./api/userRoutes'))
