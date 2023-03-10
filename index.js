0cc8666edecd91840193ddcf0c30e4a1e029d285
blitzz12 <carl.brooks1@gmail.com>
2023-03-09 22:18:31 -0700

    next

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.use(cors( {origin:true, credentials: true}))
 app.use('/api/todos', require('./api/todoRoutes'))
 app.use('/api/user', require('./api/userRoutes'))
 
-// if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static(path.join(__dirname, '../frontend/build')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  // app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))) } else {   app.get('/', (req, res) => res.send('Please set to production')) }
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))) } else {   app.get('/', (req, res) => res.send('Please set to production')) }
 
 app.use(errorHandler)
 
